f9fb07135355a93cf9fd0f14ec80f67af691cdcd
Alex <nogard171@gmail.com>
2015-02-04 13:28:31 -0500

    runnable update, to be finished for a primary game class.

--- Game/src/Game.java
+++ Game/src/Game.java
@@ -1,8 +1,16 @@
+import javax.swing.SwingUtilities;
+
 
 public class Game {
 
 	public static void main(String[] args)
 	{
+		 SwingUtilities.invokeLater(new Runnable() {     
+	            @Override
+	            public void run() {
+	               //the main game as jframe
+	            }
+	        });
 		System.out.println("hello world!");
 		while(true);
 	}
