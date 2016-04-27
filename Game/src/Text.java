ce19b34de8c998c08cf99b42adf7719307ad234f
user <laptop>
2016-04-27 18:46:09 -0400

    updated the text class to render more letters, and took out the previous test renderer.

--- Game/src/Text.java
+++ Game/src/Text.java
@@ -22,10 +22,42 @@ public class Text {
 				{ 1, 0, 0, 0, 0, 0, 1 }, 
 				{ 1, 0, 0, 0, 0, 0, 1 } };
 		alphabet2.put("A", A);
-		int[][] B = { { 1, 1, 1, 1, 1, 1, 0 }, { 1, 0, 0, 0, 0, 0, 1 }, { 1, 0, 0, 0, 0, 0, 1 },
-				{ 1, 0, 0, 0, 0, 0, 1 }, { 1, 1, 1, 1, 1, 1, 0 }, { 1, 0, 0, 0, 0, 0, 1 },
-				{ 1, 0, 0, 0, 0, 0, 1 }, { 1, 0, 0, 0, 0, 0, 1 }, { 1, 0, 0, 0, 0, 0, 1 }, { 1, 1, 1, 1, 1, 1, 0 } };
+		int[][] a = { 
+				{ 0, 0, 0, 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 1, 1, 1, 0 }, 
+				{ 0, 0, 0, 0, 0, 0, 1 }, 
+				{ 0, 0, 1, 1, 1, 1, 1 }, 
+				{ 0, 1, 0, 0, 0, 0, 1 }, 
+				{ 0, 1, 0, 0, 0, 0, 1 }, 
+				{ 0, 1, 0, 0, 0, 1, 1 }, 
+				{ 0, 0, 1, 1, 1, 0, 1 } };
+		alphabet2.put("a", a);
+		int[][] B = { 
+				{ 1, 1, 1, 1, 1, 1, 0 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0 } };
 		alphabet2.put("B", B);
+		int[][] b = { 
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 1, 1, 1, 1, 1, 0 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 0, 1, 1, 1, 1, 1, 0 } };
+		alphabet2.put("b", b);
 		int[][] C = { 
 				{ 0, 1, 1, 1, 1, 1, 0 }, 
 				{ 1, 0, 0, 0, 0, 0, 1 }, 
@@ -38,14 +70,52 @@ public class Text {
 				{ 1, 0, 0, 0, 0, 0, 1 },
 				{ 0, 1, 1, 1, 1, 1, 0 } };
 		alphabet2.put("C", C);
+		int[][] c = { 
+				{ 0, 0, 0, 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0, 0, 0, 0 }, 
+				{ 0, 0, 0, 0, 0, 0, 0 },
+				{ 0, 1, 1, 1, 1, 1, 0 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 0 },
+				{ 1, 0, 0, 0, 0, 0, 0 }, 
+				{ 1, 0, 0, 0, 0, 0, 0 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 0, 1, 1, 1, 1, 1, 0 } };
+		alphabet2.put("c", c);
+		int[][] D = { 
+				{ 1, 1, 1, 1, 1, 1, 0 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0 } };
+		alphabet2.put("D", D);
+		int[][] d = { 
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 0, 0, 0, 0, 0, 1 },
+				{ 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 }, 
+				{ 1, 0, 0, 0, 0, 0, 1 },
+				{ 0, 1, 1, 1, 1, 1, 0 } };
+		alphabet2.put("d", d);
 	}
 
-	public void Render(String s, int x, int y, Color color) {
+	public void Render(String s, int x, int y,float fontSize, Color color) {
 		y += 3;
-		x += 1;
+		x += 2;
 		GL11.glPushMatrix();
 		GL11.glColor3f(color.r, color.g, color.b);
 		int spacing = 8;
+		fontSize = 1/(8/fontSize);
+		System.out.println(fontSize);
 		for (char c : s.toCharArray()) {
 			String character = c + "";
 			GL11.glBegin(GL11.GL_QUADS);
@@ -54,10 +124,10 @@ public class Text {
 				for (int i = 0; i < characterData.length; i++) {
 					for (int j = 0; j < characterData[i].length; j++) {
 						if (characterData[i][j] == 1) {
-							GL11.glVertex2f(x + j, y + i);
-							GL11.glVertex2f(x + j + 1, y + i);
-							GL11.glVertex2f(x + j + 1, y + i + 1);
-							GL11.glVertex2f(x + j, y + i + 1);
+							GL11.glVertex2f((x*fontSize) + (j*fontSize), (y*fontSize) + (i*fontSize));
+							GL11.glVertex2f((x*fontSize) + (j*fontSize) + (1*fontSize), (y*fontSize) + (i*fontSize));
+							GL11.glVertex2f((x*fontSize) + (j*fontSize) + (1*fontSize), (y*fontSize) + (i*fontSize) + (1*fontSize));
+							GL11.glVertex2f((x*fontSize) + (j*fontSize), (y*fontSize) + (i*fontSize) + (1*fontSize));
 						}
 					}
 				}
@@ -68,479 +138,4 @@ public class Text {
 		GL11.glPopMatrix();
 	}
 
-	public static void drawString(String s, int x, int y, Color color) {
-		GL11.glColor3f(color.r, color.g, color.b);
-		int startX = x;
-		GL11.glBegin(GL11.GL_POINTS);
-		for (char c : s.toCharArray()) {
-			if (c == 'A') {
-				for (int i = 1; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == 'B') {
-				for (int i = 0; i < 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 4);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 7, y + 5);
-				GL11.glVertex2f(x + 7, y + 7);
-				GL11.glVertex2f(x + 7, y + 6);
-				GL11.glVertex2f(x + 7, y + 1);
-				GL11.glVertex2f(x + 7, y + 2);
-				GL11.glVertex2f(x + 7, y + 3);
-				x += 8;
-			} else if (c == 'C') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 6, y + 2);
-
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 6, y + 7);
-
-				x += 8;
-			} else if (c == 'D') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 6, y + 2);
-				GL11.glVertex2f(x + 6, y + 3);
-				GL11.glVertex2f(x + 6, y + 4);
-				GL11.glVertex2f(x + 6, y + 5);
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 6, y + 7);
-
-				x += 8;
-			} else if (c == 'E') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 0);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == 'F') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == 'G') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 6, y + 2);
-				GL11.glVertex2f(x + 6, y + 5);
-				GL11.glVertex2f(x + 5, y + 5);
-				GL11.glVertex2f(x + 7, y + 5);
-
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 6, y + 7);
-
-				x += 8;
-			} else if (c == 'H') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == 'I') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 3, y + i);
-				}
-				for (int i = 1; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 0);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				x += 7;
-			} else if (c == 'J') {
-				for (int i = 0; i <= 7; i++) {
-					GL11.glVertex2f(x + 6, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 1, y + 7);
-				GL11.glVertex2f(x + 1, y + 6);
-				// GL11.glVertex2f(x + 1, y + 1);
-				x += 8;
-			} else if (c == 'K') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				GL11.glVertex2f(x + 6, y);
-				GL11.glVertex2f(x + 5, y + 1);
-				GL11.glVertex2f(x + 4, y + 2);
-				GL11.glVertex2f(x + 3, y + 3);
-				GL11.glVertex2f(x + 2, y + 4);
-				GL11.glVertex2f(x + 2, y + 5);
-
-				GL11.glVertex2f(x + 3, y + 4);
-				GL11.glVertex2f(x + 4, y + 5);
-				GL11.glVertex2f(x + 5, y + 6);
-				GL11.glVertex2f(x + 6, y + 7);
-				GL11.glVertex2f(x + 7, y + 8);
-				x += 8;
-			} else if (c == 'L') {
-				for (int i = 0; i < 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				x += 7;
-			} else if (c == 'M') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				GL11.glVertex2f(x + 3, y + 2);
-				GL11.glVertex2f(x + 2, y + 1);
-				GL11.glVertex2f(x + 4, y + 3);
-
-				GL11.glVertex2f(x + 5, y + 2);
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 4, y + 3);
-				x += 8;
-			} else if (c == 'N') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				GL11.glVertex2f(x + 2, y + 2);
-				GL11.glVertex2f(x + 2, y + 2);
-				GL11.glVertex2f(x + 3, y + 3);
-				GL11.glVertex2f(x + 4, y + 4);
-				GL11.glVertex2f(x + 5, y + 5);
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 6, y + 6);
-				x += 8;
-			} else if (c == 'O') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y + 0);
-				}
-				x += 8;
-			} else if (c == 'P') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 6, y + 2);
-				GL11.glVertex2f(x + 6, y + 3);
-				x += 8;
-			} else if (c == 'Q') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					if (i != 7)
-						GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-					if (i != 6)
-						GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 4, y + 5);
-				GL11.glVertex2f(x + 5, y + 6);
-				GL11.glVertex2f(x + 6, y + 7);
-				GL11.glVertex2f(x + 7, y + 8);
-				x += 8;
-			} else if (c == 'r') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				GL11.glVertex2f(x + 6, y + 7);
-				GL11.glVertex2f(x + 6, y + 5);
-				GL11.glVertex2f(x + 6, y + 6);
-
-				GL11.glVertex2f(x + 4, y + 3);
-				GL11.glVertex2f(x + 5, y + 2);
-				GL11.glVertex2f(x + 6, y + 1);
-				GL11.glVertex2f(x + 7, y);
-				x += 8;
-			} else if (c == 's') {
-				for (int i = 2; i <= 7; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 1, y + 7);
-				GL11.glVertex2f(x + 1, y + 6);
-				GL11.glVertex2f(x + 1, y + 5);
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-					GL11.glVertex2f(x + i, y);
-				}
-				GL11.glVertex2f(x + 7, y + 3);
-				GL11.glVertex2f(x + 7, y + 2);
-				GL11.glVertex2f(x + 7, y + 1);
-				GL11.glVertex2f(x + 1, y + 1);
-				GL11.glVertex2f(x + 1, y + 2);
-				x += 8;
-			} else if (c == 't') {
-				for (int i = 0; i <= 8; i++) {
-					GL11.glVertex2f(x + 4, y + i);
-				}
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				x += 7;
-			} else if (c == 'u') {
-				for (int i = 1; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 0);
-				}
-				x += 8;
-			} else if (c == 'v') {
-				for (int i = 2; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 6, y + i);
-				}
-				GL11.glVertex2f(x + 2, y + 1);
-				GL11.glVertex2f(x + 5, y + 1);
-				GL11.glVertex2f(x + 3, y);
-				GL11.glVertex2f(x + 4, y);
-				x += 7;
-			} else if (c == 'w') {
-				for (int i = 1; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				GL11.glVertex2f(x + 2, y);
-				GL11.glVertex2f(x + 3, y);
-				GL11.glVertex2f(x + 5, y);
-				GL11.glVertex2f(x + 6, y);
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + 4, y + i);
-				}
-				x += 8;
-			} else if (c == 'x') {
-				for (int i = 1; i <= 7; i++)
-					GL11.glVertex2f(x + i, y + i);
-				for (int i = 7; i >= 1; i--)
-					GL11.glVertex2f(x + i, y + 8 - i);
-				x += 8;
-			} else if (c == 'y') {
-				GL11.glVertex2f(x + 4, y);
-				GL11.glVertex2f(x + 4, y + 1);
-				GL11.glVertex2f(x + 4, y + 2);
-				GL11.glVertex2f(x + 4, y + 3);
-				GL11.glVertex2f(x + 4, y + 4);
-
-				GL11.glVertex2f(x + 3, y + 5);
-				GL11.glVertex2f(x + 2, y + 6);
-				GL11.glVertex2f(x + 1, y + 7);
-				GL11.glVertex2f(x + 1, y + 8);
-
-				GL11.glVertex2f(x + 5, y + 5);
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 7, y + 7);
-				GL11.glVertex2f(x + 7, y + 8);
-				x += 8;
-			} else if (c == 'z') {
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y + i);
-				}
-				GL11.glVertex2f(x + 6, y + 7);
-				x += 8;
-			} else if (c == '1') {
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-				}
-				for (int i = 1; i <= 8; i++) {
-					GL11.glVertex2f(x + 4, y + i);
-				}
-				GL11.glVertex2f(x + 3, y + 7);
-				x += 8;
-			} else if (c == '2') {
-				for (int i = 1; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 1, y + 7);
-				GL11.glVertex2f(x + 1, y + 6);
-
-				GL11.glVertex2f(x + 6, y + 7);
-				GL11.glVertex2f(x + 6, y + 6);
-				GL11.glVertex2f(x + 6, y + 5);
-				GL11.glVertex2f(x + 5, y + 4);
-				GL11.glVertex2f(x + 4, y + 3);
-				GL11.glVertex2f(x + 3, y + 2);
-				GL11.glVertex2f(x + 2, y + 1);
-				x += 8;
-			} else if (c == '3') {
-				for (int i = 1; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y);
-				}
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 6, y + i);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == '4') {
-				for (int i = 2; i <= 8; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 7; i++) {
-					GL11.glVertex2f(x + i, y + 1);
-				}
-				for (int i = 0; i <= 4; i++) {
-					GL11.glVertex2f(x + 4, y + i);
-				}
-				x += 8;
-			} else if (c == '5') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				for (int i = 4; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				GL11.glVertex2f(x + 1, y + 1);
-				GL11.glVertex2f(x + 2, y);
-				GL11.glVertex2f(x + 3, y);
-				GL11.glVertex2f(x + 4, y);
-				GL11.glVertex2f(x + 5, y);
-				GL11.glVertex2f(x + 6, y);
-
-				GL11.glVertex2f(x + 7, y + 1);
-				GL11.glVertex2f(x + 7, y + 2);
-				GL11.glVertex2f(x + 7, y + 3);
-
-				GL11.glVertex2f(x + 6, y + 4);
-				GL11.glVertex2f(x + 5, y + 4);
-				GL11.glVertex2f(x + 4, y + 4);
-				GL11.glVertex2f(x + 3, y + 4);
-				GL11.glVertex2f(x + 2, y + 4);
-				x += 8;
-			} else if (c == '6') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y);
-				}
-				for (int i = 2; i <= 5; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-					GL11.glVertex2f(x + i, y + 8);
-				}
-				GL11.glVertex2f(x + 7, y + 1);
-				GL11.glVertex2f(x + 7, y + 2);
-				GL11.glVertex2f(x + 7, y + 3);
-				GL11.glVertex2f(x + 6, y + 4);
-				x += 8;
-			} else if (c == '7') {
-				for (int i = 0; i <= 7; i++)
-					GL11.glVertex2f(x + i, y + 8);
-				GL11.glVertex2f(x + 7, y + 7);
-				GL11.glVertex2f(x + 7, y + 6);
-
-				GL11.glVertex2f(x + 6, y + 5);
-				GL11.glVertex2f(x + 5, y + 4);
-				GL11.glVertex2f(x + 4, y + 3);
-				GL11.glVertex2f(x + 3, y + 2);
-				GL11.glVertex2f(x + 2, y + 1);
-				GL11.glVertex2f(x + 1, y);
-				x += 8;
-			} else if (c == '8') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y + 0);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				x += 8;
-			} else if (c == '9') {
-				for (int i = 1; i <= 7; i++) {
-					GL11.glVertex2f(x + 7, y + i);
-				}
-				for (int i = 5; i <= 7; i++) {
-					GL11.glVertex2f(x + 1, y + i);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 8);
-					GL11.glVertex2f(x + i, y + 0);
-				}
-				for (int i = 2; i <= 6; i++) {
-					GL11.glVertex2f(x + i, y + 4);
-				}
-				GL11.glVertex2f(x + 1, y + 0);
-				x += 8;
-			} else if (c == '.') {
-				GL11.glVertex2f(x + 1, y);
-				x += 2;
-			} else if (c == ',') {
-				GL11.glVertex2f(x + 1, y);
-				GL11.glVertex2f(x + 1, y + 1);
-				x += 2;
-			} else if (c == '\n') {
-				y -= 10;
-				x = startX;
-			} else if (c == ' ') {
-				x += 8;
-			}
-		}
-		GL11.glEnd();
-	}
 }
