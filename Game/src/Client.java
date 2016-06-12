dace23d01172f523b2eaef95b7a7cfd0852ffe83
Nogard <Nogard@DESKTOP>
2016-06-12 12:36:35 -0400

    updating network map to chunk based rendering, to allow for infinite map.
    chunk based rendering will allow prevent server from init load time.

--- Game/src/Client.java
+++ Game/src/Client.java
@@ -16,193 +16,256 @@ import org.lwjgl.opengl.Display;
 import Objects.Account;
 import Objects.MapData;
 import Objects.NetworkData;
+import Objects.ObjectData;
 
-public class Client extends Thread {
-	static String sentence;
-	static String modifiedSentence;
+public class Client extends Thread
+{
+    static String	    sentence;
+    static String	    modifiedSentence;
 
-	static Socket clientSocket = null;
-	static BufferedReader inFromUser = null;
-	static DataOutputStream outToServer = null;
-	static DataInputStream inFromServer = null;
-	public String error = "";
-	private int port = 2222;
+    static Socket	    clientSocket = null;
+    static BufferedReader   inFromUser	 = null;
+    static DataOutputStream outToServer	 = null;
+    static DataInputStream  inFromServer = null;
+    public String	    error	 = "";
+    private int		    port	 = 2222;
 
-	public void initilize() throws UnknownHostException, IOException {
-		clientSocket = new Socket("localhost", port);
+    public void initilize() throws UnknownHostException, IOException
+    {
+	clientSocket = new Socket("localhost", port);
+    }
+
+    boolean    logged_in = false;
+    public Map map	 = null;
+
+    public Boolean login(String username, String password)
+    {
+	if (error == "")
+	{
+
+	    String hashtext = getHash(password);
+
+	    NetworkData newData = new NetworkData("LOGIN");
+
+	    Account newAccount = new Account(username, hashtext);
+	    newData.account = newAccount;
+	    SendData(newData);
+	    System.out.println("Login Request Sent.");
+	    try
+	    {
+		newData = getData();
+	    }
+	    catch (ClassNotFoundException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    catch (IOException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    if (newData == null)
+	    {
+		System.out.println("failed");
+	    }
+	    System.out.println("Login Request Received.");
+	    if (newData.command.equals("OK"))
+	    {
+		this.map = new Map();
+		System.out.println("Login Successful.");
+		int newWidth = Math.round((Display.getWidth() / 32));
+		int newHeight = Math.round((Display.getHeight() / 32)) + 1;
+		getMap(0, 0, newHeight, newWidth);
+		logged_in = true;
+	    }
+	    else if (newData.command.equals("FAILED"))
+	    {
+		System.out.println("Login Unsuccessful:" + newData.message);
+		newData.message = "";
+		logged_in = false;
+	    }
+
+	}
+	else
+	{
+	    JOptionPane.showMessageDialog(null, error, "Error", JOptionPane.ERROR_MESSAGE);
+	}
+	return logged_in;
+    }
+
+    public void getMap(int i, int j, int newHeight, int newWidth)
+    {
+	// TODO Auto-generated method stub
+	NetworkData mapdata = new NetworkData("MAP");
+	mapdata.map = new MapData(i, j, newWidth, newHeight);
+	SendData(mapdata);
+    }
+
+    public String getHash(String text)
+    {
+	MessageDigest md;
+	String hashtext = "";
+	try
+	{
+	    md = MessageDigest.getInstance("MD5");
+	    byte[] messageDigest = md.digest(text.getBytes());
+	    BigInteger number = new BigInteger(1, messageDigest);
+	    hashtext = number.toString(16);
+	}
+	catch (NoSuchAlgorithmException e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	return hashtext;
+    }
+
+    public void run()
+    {
+	HashMap<String, String> options = new Config().getConfig("network");
+	this.port = Integer.parseInt(options.get("port"));
+	try
+	{
+	    initilize();
+	}
+	catch (ConnectException err)
+	{
+	    error = err.getMessage();
+	}
+	catch (UnknownHostException e)
+	{
+	    // TODO Auto-generated catch block
+	    error = e.getMessage();
+	}
+	catch (IOException e)
+	{
+	    // TODO Auto-generated catch block
+	    error = e.getMessage();
+	}
+	System.out.println("Initilized Connection.");
+	clientLoop();
+	System.out.println("Loop Terminated.");
+	closeClient();
+	System.out.println("Connection Terminated.");
+    }
+
+    public int mapCount = 0;
+
+    public void clientLoop()
+    {
+	while (!logged_in)
+	{
+	    try
+	    {
+		Thread.sleep(10);
+	    }
+	    catch (InterruptedException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
 	}
 
-	boolean logged_in = false;
-	public Map map = null;
+	while (logged_in)
+	{
+	    NetworkData data = new NetworkData("");
+	    try
+	    {
+		data = getData();
+	    }
+	    catch (ClassNotFoundException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    catch (IOException e)
+	    {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	    String command = data.command;
 
-	public Boolean login(String username, String password) {
-		if (error == "") {
-
-			String hashtext = getHash(password);
-
-			NetworkData newData = new NetworkData("LOGIN");
-
-			Account newAccount = new Account(username, hashtext);
-			newData.account = newAccount;
-			SendData(newData);
-			System.out.println("Login Request Sent.");
-			try {
-				newData = getData();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+	    if (data.command.startsWith("TILE"))
+	    {
+		if (data.map.chunks.containsKey(data.map.x + "," + data.map.y))
+		{
+		    for (int y = 0; y < 32; y++)
+		    {
+			for (int x = 0; x < 32; x++)
+			{
+			    ObjectData tile =data.map.chunks.get(data.map.x + "," + data.map.y).tiles
+				    .get((x * 32) + "," + (y * 32));
+			    if (tile != null)
+			    {
+				this.map.addTile(tile);
+				System.out.println("tile("+(x * 32) + "," + (y * 32)+")");
+			    }
 			}
-			if (newData == null) {
-				System.out.println("failed");
-			}
-			System.out.println("Login Request Received.");
-			if (newData.command.equals("OK")) {
-				this.map = new Map(10, 10);
-				this.map.count = newData.map.count;
-				System.out.println("Login Successful.");
-				int newWidth = Math.round((Display.getWidth() / 32));
-				int newHeight = Math.round((Display.getHeight() / 32)) + 1;
-				getMap(0, 0, newHeight, newWidth);
-				logged_in = true;
-			} else if (newData.command.equals("FAILED")) {
-				System.out.println("Login Unsuccessful:" + newData.message);
-				newData.message = "";
-				logged_in = false;
-			}
-
-		} else {
-			JOptionPane.showMessageDialog(null, error, "Error", JOptionPane.ERROR_MESSAGE);
+		    }
 		}
-		return logged_in;
+		/*
+		 * for (int i = 0; i < data.map.objectArray.size(); i++) {
+		 * if (data.map.objectArray.get(i) != null) {
+		 * this.map.addObject(data.map.objectArray.get(i));
+		 * }
+		 * }
+		 */
+	    }
+	    if (command.startsWith("CLOSE/C"))
+	    {
+		break;
+	    }
 	}
+    }
 
-	public void getMap(int i, int j, int newHeight, int newWidth) {
-		// TODO Auto-generated method stub
-		NetworkData mapdata = new NetworkData("MAP");
-		mapdata.map = new MapData(i, j, newWidth, newHeight);
-		SendData(mapdata);
+    public void closeClient()
+    {
+
+	try
+	{
+	    clientSocket.close();
 	}
-
-	public String getHash(String text) {
-		MessageDigest md;
-		String hashtext = "";
-		try {
-			md = MessageDigest.getInstance("MD5");
-			byte[] messageDigest = md.digest(text.getBytes());
-			BigInteger number = new BigInteger(1, messageDigest);
-			hashtext = number.toString(16);
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return hashtext;
+	catch (IOException e)
+	{
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+	System.out.println("Client closed:\n" + error);
+	System.exit(0);
+    }
 
-	public void run() {
-		HashMap<String, String> options = new Config().getConfig("network");
-		this.port = Integer.parseInt(options.get("port"));
-		try {
-			initilize();
-		} catch (ConnectException err) {
-			error = err.getMessage();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			error = e.getMessage();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			error = e.getMessage();
-		}
-		System.out.println("Initilized Connection.");
-		clientLoop();
-		System.out.println("Loop Terminated.");
-		closeClient();
-		System.out.println("Connection Terminated.");
+    ObjectOutputStream oos = null;
+
+    public void SendData(NetworkData data)
+    {
+	try
+	{
+	    oos = new ObjectOutputStream(clientSocket.getOutputStream());
+
+	    oos.writeObject(data);
+	    oos.flush();
+
+	    // DataOutputStream(clientSocket.getOutputStream());
+	    // outToServer.writeUTF(message);
 	}
-
-	public int mapCount = 0;
-
-	public void clientLoop() {
-		while (!logged_in) {
-			try {
-				Thread.sleep(10);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-
-		while (logged_in) {
-			NetworkData data = new NetworkData("");
-			try {
-				data = getData();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			String command = data.command;
-			
-			if (data.command.startsWith("TILE")) {
-				for (int i = 0; i < data.map.ground.size(); i++) {
-					if (data.map.ground.get(i) != null) {
-						this.map.addTile(data.map.ground.get(i));
-					}
-				}
-				for (int i = 0; i < data.map.objectArray.size(); i++) {
-					if (data.map.objectArray.get(i) != null) {
-						this.map.addObject(data.map.objectArray.get(i));
-					}
-				}
-			}
-			if (command.startsWith("CLOSE/C")) {
-				break;
-			}
-		}
+	catch (IOException e)
+	{
+	    // TODO Auto-generated catch block
+	    error = e.getLocalizedMessage();
 	}
+    }
 
-	public void closeClient() {
+    ObjectInputStream ois = null;
 
-		try {
-			clientSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("Client closed:\n" + error);
-		System.exit(0);
-	}
+    public NetworkData getData() throws IOException, ClassNotFoundException
+    {
+	NetworkData newData = new NetworkData("");
 
-	ObjectOutputStream oos = null;
+	ois = new ObjectInputStream(clientSocket.getInputStream());
 
-	public void SendData(NetworkData data) {
-		try {
-			oos = new ObjectOutputStream(clientSocket.getOutputStream());
-
-			oos.writeObject(data);
-			oos.flush();
-
-			// DataOutputStream(clientSocket.getOutputStream());
-			// outToServer.writeUTF(message);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			error = e.getLocalizedMessage();
-		}
-	}
-
-	ObjectInputStream ois = null;
-
-	public NetworkData getData() throws IOException, ClassNotFoundException {
-		NetworkData newData = new NetworkData("");
-
-		ois = new ObjectInputStream(clientSocket.getInputStream());
-
-		newData = (NetworkData) ois.readObject();
-		return newData;
-	}
+	newData = (NetworkData) ois.readObject();
+	return newData;
+    }
 
 }
--- Game/src/Map.java
+++ Game/src/Map.java
@@ -21,26 +21,9 @@ public class Map extends MapData
 {
     HashMap<String, Tile>   mapTiles   = new HashMap<String, Tile>();
     HashMap<String, Entity> mapObjects = new HashMap<String, Entity>();
-
-    public Map(ArrayList<ObjectData> ground)
-    {
-	this.ground = ground;
-    }
-
-    public Map(HashMap<String, ObjectData> tiles)
-    {
-	this.tiles = tiles;
-    }
-
     public Map()
     {
 	// TODO Auto-generated constructor stub
-    }
-
-    public Map(int width, int height)
-    {
-	this.width = width;
-	this.height = height;
     }
 
     public Texture loadTexture(String texture_url)
@@ -124,50 +107,51 @@ public class Map extends MapData
 
     public void RenderBottom(int playerX, int playerY)
     {
-	int newWidth = Math.round((Display.getWidth() / 32)) + 3;
-	int newHeight = Math.round((Display.getHeight() / 32)) + 3;
+	//System.out.println("Count:"+mapTiles.size());
+	int newWidth = Math.round((Display.getWidth() / 1024)) + 3;
+	int newHeight = Math.round((Display.getHeight() / 1024)) + 3;
 	int count = 0;
 	int texturedCount = 0;
-	int newX = (playerX / 32) - 1;
-	int newY = (playerY / 32) - 1;
-	if (mapTiles.size() > 0)
+	int newX = (playerX / 1024) - 1;
+	int newY = (playerY / 1024) - 1;
+	for (int x = 0; x < 32; x++)
 	{
-	    for (int x = newX; x < (newX + newWidth); x++)
+	    for (int y = 0; y < 32; y++)
 	    {
-		for (int y = newY; y < newY + newHeight; y++)
+		String key = (x * 32) + "," + (y * 32);
+
+		if (mapTiles.containsKey(key))
 		{
-		    String key = (x * 32) + "," + (y * 32);
-		    if (mapTiles.containsKey(key))
+			
+		    if (mapTiles.get(key) != null)
 		    {
-			if (mapTiles.get(key) != null)
+			if (mapTiles.get(key).type != ObjectType.OTHER && mapTiles.get(key).texture == null
+				&& texturedCount <= (newWidth + newHeight) / 2)
 			{
-			    if (mapTiles.get(key).type != ObjectType.OTHER && mapTiles.get(key).texture == null
-				    && texturedCount <= (newWidth + newHeight) / 2)
-			    {
-				mapTiles.get(key).texture = getTexture(mapTiles.get(key).type);
-				texturedCount++;
-			    }
-			    mapTiles.get(key).Render();
-			    count++;
+			    mapTiles.get(key).texture = getTexture(mapTiles.get(key).type);
+			    texturedCount++;
 			}
-		    }
-		    if (mapObjects.containsKey(key))
-		    {
-			if (mapObjects.get(key) != null)
-			{
-			    if (mapObjects.get(key).type != ObjectType.OTHER && mapObjects.get(key).texture == null
-				    && texturedCount <= (newWidth + newHeight) / 2)
-			    {
-				mapObjects.get(key).texture = getTexture(mapObjects.get(key).type);
-				texturedCount++;
-			    }
-			    mapObjects.get(key).Render();
-			    count++;
-			}
+			mapTiles.get(key).Render();
+			count++;
 		    }
 		}
+		/*if (mapObjects.containsKey(key))
+		{
+		    if (mapObjects.get(key) != null)
+		    {
+			if (mapObjects.get(key).type != ObjectType.OTHER && mapObjects.get(key).texture == null
+				&& texturedCount <= (newWidth + newHeight) / 2)
+			{
+			    mapObjects.get(key).texture = getTexture(mapObjects.get(key).type);
+			    texturedCount++;
+			}
+			mapObjects.get(key).Render();
+			count++;
+		    }
+		}*/
 	    }
 	}
+
     }
 
     public Texture getTexture(ObjectType objType)
@@ -214,10 +198,25 @@ public class Map extends MapData
 	switch (objType)
 	{
 	    case GRASS:
-		color = new Color(77, 134, 53);
+		  color = new Color(74, 131, 50);
 		break;
 	    case DIRT:
 		color = new Color(139, 69, 19);
+		break;
+	    case SAND:
+		color = new Color(255, 242, 170);
+		break;
+	    case DEEPWATER:
+		 color = new Color(0, 94, 158);
+		break;
+	    case WATER:
+		 color = new Color(22, 130, 194);
+		break;
+	    case STONE:
+		 color = new Color(198, 198, 198);
+		break;
+	    case SNOW:
+		color = new Color(239, 239, 239);
 		break;
 	    default:
 		break;
