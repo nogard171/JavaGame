87daf77a426d2278cef720188866a8f843c3d7aa
User <User@DESKTOP-KKBGD0E>
2016-11-18 20:08:50 -0500

    ThinMatrix Tutorial

---
+++ Game/src/renderEngine/RawModel.java
@@ -0,0 +1,34 @@
+package renderEngine;
+
+public class RawModel
+{
+	private int vaoID;
+	private int vertexCount;
+	
+	public int getVertexCount()
+	{
+		return vertexCount;
+	}
+
+	public void setVertexCount(int vertexCount)
+	{
+		this.vertexCount = vertexCount;
+	}
+
+	public RawModel(int vaoID,int vertexCount)
+	{
+		this.vaoID = vaoID;
+		this.vertexCount = vertexCount;		
+	}
+
+	public int getVaoID()
+	{
+		return vaoID;
+	}
+
+	public void setVaoID(int vaoID)
+	{
+		this.vaoID = vaoID;
+	}
+
+}
