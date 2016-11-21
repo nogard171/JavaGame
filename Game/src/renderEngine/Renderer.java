a18f01f71acf4e11b38c98233ce9eebd83a4e411
User <User@DESKTOP-KKBGD0E>
2016-11-20 19:33:20 -0500

    on tutorial 6

--- Game/src/renderEngine/Renderer.java
+++ Game/src/renderEngine/Renderer.java
@@ -8,6 +8,7 @@ public class Renderer
 {
 	public void prepare()
 	{
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 		GL11.glClearColor(1, 0, 0, 1);
 	}
 
@@ -15,7 +16,7 @@ public class Renderer
 	{
 		GL30.glBindVertexArray(model.getVaoID());
 		GL20.glEnableVertexAttribArray(0);
-		GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, model.getVertexCount());
+		GL11.glDrawElements(GL11.GL_TRIANGLES,model.getVertexCount(),GL11.GL_UNSIGNED_INT,0);
 		GL20.glDisableVertexAttribArray(0);
 		GL30.glBindVertexArray(0);
 	}
