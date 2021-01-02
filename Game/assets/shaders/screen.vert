3d60c070b445be0213e5b79de4737ae11399bb78
AlexS <AlexS@192.168.2.93>
2021-01-01 20:55:40 -0500

    test

--- Game/assets/shaders/screen.vert
+++ Game/assets/shaders/screen.vert
@@ -1,15 +1,14 @@
 #version 110
-
 uniform vec2 position;
 
-varying vec4 vertColor;
-
-void main(){
+void main(void)
+{
 	mat4x4 pos=mat4x4(1.0);
     pos[3].x=position.x;
     pos[3].y=position.y;
-    
-    
-    gl_Position = gl_ModelViewProjectionMatrix*gl_Vertex*pos;
-    vertColor = vec4(0.6, 0.3, 0.4, 1.0);
+
+
+	//gl_TexCoord[0] = gl_MultiTexCoord0;
+// 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+ 	gl_Position = gl_ModelViewProjectionMatrix*pos*gl_Vertex;
 }
--- Game/src/threads/GameThread.java
+++ Game/src/threads/GameThread.java
@@ -20,141 +20,16 @@ import java.nio.IntBuffer;
 import org.lwjgl.util.vector.Vector2f;
 
 import core.Input;
+import core.ShaderProgram;
 import utils.FPS;
 
 public class GameThread extends BaseThread {
-	private int program = 0;
-	boolean useShader = false;
+	ShaderProgram shader;
 
 	@Override
 	public void init() {
 		super.init();
-		int vertShader = 0, fragShader = 0;
-
-		try {
-			vertShader = createShader("assets/shaders/screen.vert", ARBVertexShader.GL_VERTEX_SHADER_ARB);
-			fragShader = createShader("assets/shaders/screen.frag", ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
-		} catch (Exception exc) {
-			exc.printStackTrace();
-			return;
-		} finally {
-			if (vertShader == 0 || fragShader == 0)
-				return;
-		}
-
-		program = ARBShaderObjects.glCreateProgramObjectARB();
-
-		if (program == 0)
-			return;
-
-		/*
-		 * if the vertex and fragment shaders setup sucessfully, attach them to the
-		 * shader program, link the sahder program (into the GL context I suppose), and
-		 * validate
-		 */
-		ARBShaderObjects.glAttachObjectARB(program, vertShader);
-		ARBShaderObjects.glAttachObjectARB(program, fragShader);
-
-		ARBShaderObjects.glLinkProgramARB(program);
-		if (ARBShaderObjects.glGetObjectParameteriARB(program,
-				ARBShaderObjects.GL_OBJECT_LINK_STATUS_ARB) == GL11.GL_FALSE) {
-			System.err.println(getLogInfo(program));
-			return;
-		}
-
-		ARBShaderObjects.glValidateProgramARB(program);
-		if (ARBShaderObjects.glGetObjectParameteriARB(program,
-				ARBShaderObjects.GL_OBJECT_VALIDATE_STATUS_ARB) == GL11.GL_FALSE) {
-			System.err.println(getLogInfo(program));
-			return;
-		}
-		useShader = true;
-
-	}
-
-	private int createShader(String filename, int shaderType) throws Exception {
-		int shader = 0;
-		try {
-			shader = ARBShaderObjects.glCreateShaderObjectARB(shaderType);
-
-			if (shader == 0)
-				return 0;
-
-			ARBShaderObjects.glShaderSourceARB(shader, readFileAsString(filename));
-			ARBShaderObjects.glCompileShaderARB(shader);
-
-			if (ARBShaderObjects.glGetObjectParameteriARB(shader,
-					ARBShaderObjects.GL_OBJECT_COMPILE_STATUS_ARB) == GL11.GL_FALSE)
-				throw new RuntimeException("Error creating shader: " + getLogInfo(shader));
-
-			return shader;
-		} catch (Exception exc) {
-			ARBShaderObjects.glDeleteObjectARB(shader);
-			throw exc;
-		}
-	}
-
-	private static String getLogInfo(int obj) {
-		return ARBShaderObjects.glGetInfoLogARB(obj,
-				ARBShaderObjects.glGetObjectParameteriARB(obj, ARBShaderObjects.GL_OBJECT_INFO_LOG_LENGTH_ARB));
-	}
-
-	private String readFileAsString(String filename) throws Exception {
-		StringBuilder source = new StringBuilder();
-
-		FileInputStream in = new FileInputStream(filename);
-
-		Exception exception = null;
-
-		BufferedReader reader;
-		try {
-			reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-
-			Exception innerExc = null;
-			try {
-				String line;
-				while ((line = reader.readLine()) != null)
-					source.append(line).append('\n');
-			} catch (Exception exc) {
-				exception = exc;
-			} finally {
-				try {
-					reader.close();
-				} catch (Exception exc) {
-					if (innerExc == null)
-						innerExc = exc;
-					else
-						exc.printStackTrace();
-				}
-			}
-
-			if (innerExc != null)
-				throw innerExc;
-		} catch (Exception exc) {
-			exception = exc;
-		} finally {
-			try {
-				in.close();
-			} catch (Exception exc) {
-				if (exception == null)
-					exception = exc;
-				else
-					exc.printStackTrace();
-			}
-
-			if (exception != null)
-				throw exception;
-		}
-
-		return source.toString();
-	}
-
-	public void setUniformVariables(int programID, Vector2f position) {
-
-		int loc2 = GL20.glGetUniformLocation(programID, "playerPos");
-		GL20.glUniform2f(loc2, position.x, position.y);
-		// You read the previous comment right?
-
+		shader = new ShaderProgram("assets/shaders/screen.vert", "assets/shaders/screen.frag");
 	}
 
 	@Override
@@ -183,27 +58,24 @@ public class GameThread extends BaseThread {
 	@Override
 	public void render() {
 		super.render();
-		if (useShader) {
-			ARBShaderObjects.glUseProgramObjectARB(program);
-		}
-		setUniformVariables(program, pos);
+		shader.Run();
+		float[] position = { pos.x, pos.y };
+		shader.sendUniform2f("position", position);
 		// GL11.glColor3f(1, 0, 0);
 		GL11.glBegin(GL11.GL_QUADS);
 		GL11.glVertex2f(0, 0);
 		GL11.glVertex2f(32, 0);
-		GL11.glVertex2f(32, 32 );
+		GL11.glVertex2f(32, 32);
 		GL11.glVertex2f(0, 32);
 
-		setUniformVariables(program, new Vector2f(100, 100));
+		float[] position2 = { 100, 100 };
+		shader.sendUniform2f("position", position2);
 		GL11.glVertex2f(0, 0);
 		GL11.glVertex2f(32, 0);
-		GL11.glVertex2f(32, 32 );
+		GL11.glVertex2f(32, 32);
 		GL11.glVertex2f(0, 32);
 		GL11.glEnd();
-
-		if (useShader) {
-			ARBShaderObjects.glUseProgramObjectARB(0);
-		}
+		shader.Stop();
 	}
 
 	@Override
